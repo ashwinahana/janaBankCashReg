54dd350aac56b1161895159686f43c19e25200b4
ashwinkalyanpur <akalyanpur1974@gmail.com>
2020-11-23 01:50:42 +0530

    Add application

--- ApplicationCashReg/src/app/app.module.ts
+++ ApplicationCashReg/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CollectionComponent } from './collection/collection.component';
+import { HeaderCollectionComponent } from './collection/header-collection/header-collection.component';
+import { LeftMenuComponent } from './left-menu/left-menu.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CollectionComponent,
+    HeaderCollectionComponent,
+    LeftMenuComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ ApplicationCashReg/src/app/collection/header-collection/header-collection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-collection',
+  templateUrl: './header-collection.component.html',
+  styleUrls: ['./header-collection.component.css']
+})
+export class HeaderCollectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
